c0fd3a5c16334329182c666c0c7f61725de8f50c
Darsh yadav <darsh.yadav@proton.me>
2025-12-19 14:40:04 +0530

    nvm

--- modules/packages/nvf/config/base.nix
+++ modules/packages/nvf/config/base.nix
@@ -8,6 +8,7 @@
         name = "catppuccin";
         style = "mocha";
       };
+
       clipboard = {
         enable = true;
         providers.wl-copy.enable = true;
@@ -15,30 +16,6 @@
       luaConfigRC.base = ''
         vim.opt.clipboard = "unnamedplus"
         vim.opt.fillchars = { eob = " " }
-
-        vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#474b59" })
-        vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#474b59" })
-        vim.api.nvim_set_hl(0, "LineNr", { fg = "#ffb778" })
-        
-        vim.api.nvim_create_augroup("NumberToggle", { clear = true })
-        
-        vim.api.nvim_create_autocmd("InsertEnter", {
-            group = "NumberToggle",
-            pattern = "*",
-            callback = function()
-                vim.opt.relativenumber = false
-                vim.api.nvim_set_hl(0, "LineNr", { fg = "#474b59" })
-            end,
-        })
-        
-        vim.api.nvim_create_autocmd("InsertLeave", {
-            group = "NumberToggle",
-            pattern = "*",
-            callback = function()
-                vim.opt.relativenumber = true
-                vim.api.nvim_set_hl(0, "LineNr", { fg = "#ffb778" })
-            end,
-        })
       '';
       keymaps = [
         {
