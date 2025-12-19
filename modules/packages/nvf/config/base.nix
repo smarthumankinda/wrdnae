0971b31467be83b0575cfa9b3e964386a87d8597
Darsh yadav <darsh.yadav@proton.me>
2025-12-19 18:45:08 +0530

    this is just like yeah no I don't want bs

--- modules/packages/nvf/config/base.nix
+++ modules/packages/nvf/config/base.nix
@@ -1,36 +1,36 @@
 {
-  flake.modules.nvf.base = {
-    config.vim = {
-      treesitter.enable = true;
-      options.shiftwidth = 2;
-      theme = {
-        enable = true;
-        name = "catppuccin";
-        style = "mocha";
-      };
+    flake.modules.nvf.base = {
+        config.vim = {
+            treesitter.enable = true;
+            options.shiftwidth = 4;
+            theme = {
+                enable = true;
+                name = "everforest";
+                style = "hard";
+            };
 
-      clipboard = {
-        enable = true;
-        providers.wl-copy.enable = true;
-      };
-      luaConfigRC.base = ''
+            clipboard = {
+                enable = true;
+                providers.wl-copy.enable = true;
+            };
+            luaConfigRC.base = ''
         vim.opt.clipboard = "unnamedplus"
         vim.opt.fillchars = { eob = " " }
-      '';
-      keymaps = [
-        {
-          key = "<C-d>";
-          mode = "n";
-          silent = true;
-          action = "<C-d>zz";
-        }
-        {
-          key = "<C-u>";
-          mode = "n";
-          silent = true;
-          action = "<C-u>zz";
-        }
-      ];
+            '';
+            keymaps = [
+                {
+                    key = "<C-d>";
+                    mode = "n";
+                    silent = true;
+                    action = "<C-d>zz";
+                }
+                {
+                    key = "<C-u>";
+                    mode = "n";
+                    silent = true;
+                    action = "<C-u>zz";
+                }
+            ];
+        };
     };
-  };
 }
--- modules/packages/nvf/config/plugs.nix
+++ modules/packages/nvf/config/plugs.nix
@@ -2,7 +2,7 @@
   flake.modules.nvf.plugs = {
     vim = {
       statusline.lualine = {
-        enable = true;
+        enable = false;
       };
       notes.todo-comments.enable = true;
       dashboard.alpha.enable = true;
@@ -11,10 +11,6 @@
       autocomplete.blink-cmp.enable = true;
       visuals = {
         indent-blankline.enable = true;
-        rainbow-delimiters.enable = true;
-      };
-      mini = {
-        indentscope.enable = true;
       };
       binds = {
         whichKey.enable =true;
